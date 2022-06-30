f01a508e7daa672854be735beb5ae81be6939ae5
Blaine Simpson <blaine.simpson@admc.com>
2022-06-30 09:31:11 -0400

    fully satisfied local self-lint check

--- masterTest.js
+++ masterTest.js
@@ -1,4 +1,6 @@
-if (!__filename.startsWith(process.cwd() + "/"))
+"use strict";
+/* global it */
+if (!__filename.startsWith(process.cwd() + "/"))  // eslint-disable-line prefer-template
     throw new Error("You must execute these tests from the package base directory");
 const relTestPath = __filename.substring(process.cwd().length + 1);
 const path = require("path");
@@ -8,6 +10,7 @@ if (!ex) throw new Error(`Bad test file path: ${relTestPath}`);
 const shouldSucceed = ex[1] === "good";
 const altscope = ex[2];
 const tableBase = ex[3];
+// eslint-disable-next-line no-extra-parens, prefer-template
 const scriptPath = path.join(`${ex[1]}Scripts`, `${altscope?(altscope+"/"):""}${tableBase}.js`);
 if (!fs.existsSync(scriptPath)) throw new Error(`Script file missing: ${scriptPath}`);
 if (!fs.statSync(scriptPath).isFile()) throw new Error(`Script is not a file: ${scriptPath}`);
@@ -21,7 +24,7 @@ const childProcess = require("child_process").
   spawnSync(process.execPath, params, { stdio: "inherit" });
 
 it(altscope ? `${tableBase} | ${altscope}` : tableBase, () => {
-    if ((shouldSucceed && childProcess.status !== 0)
-      || (!shouldSucceed && childProcess.status === 0))
+    if (shouldSucceed && childProcess.status !== 0
+      || !shouldSucceed && childProcess.status === 0)
         throw new Error(`Failed with code ${childProcess.status}`);
 });
