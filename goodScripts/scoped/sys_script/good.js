2d2c4a1cf8d8cc92f50f67d5940dbef04325a024
Blaine Simpson <blaine.simpson@admc.com>
2022-07-01 10:57:01 -0400

    optimizations

--- goodScripts/scoped/sys_script/good.js
+++ goodScripts/scoped/sys_script/good.js
@@ -1,8 +1,6 @@
 // Test availability of scoped vars from the different component lists:
-/* eslint-disable camelcase */
 gs.info(new sn_ws_err.NotAcceptableError("fake err"));  // coreServerObjects.txt
 gs.info(Object.keys(new sn_codesearch.CodeSearch()).length);  // SIScopes
-/* eslint-enable camelcase */
 
 var varsAreAllowed = "a val";
 gs.info(varsAreAllowed);
