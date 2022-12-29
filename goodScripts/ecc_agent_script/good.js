97f98478a014be743f1a1730694bf8153efa7731
Blaine Simpson <blaine.simpson@admc.com>
2022-12-29 15:30:45 -0500

    Updated for eslint-plugin-sn major version 3

--- goodScripts/ecc_agent_script/good.js
+++
@@ -1,6 +0,0 @@
-const TstCls = Class.create();
-TstCls.prototype.initialize = function(o) {
-    this.o = o;
-    this.p = String(Packages.java.lang.System.getProperty(o));
-};
-ctx.setAttribute("A", "aye");
