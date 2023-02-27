eba5a2c548a37a368bae20b725166e3d2f56504e
Blaine Simpson <blaine.simpson@admc.com>
2023-02-27 01:58:42 -0500

    Testing eslint-plugin-sn 3.4.  Just need to add negative tests for sys_ux_client_script.

---
+++ goodScripts/all/sys_ux_client_script/ArrowFnNoParams.js
@@ -0,0 +1,2 @@
+// eslint-disable-next-line no-empty-pattern
+({}) => { console.info("No params"); };
---
+++ goodScripts/all/sys_ux_client_script/tradIncompleteAnonyFn.js
@@ -0,0 +1,5 @@
+// Header comment
+function({a, b}) {
+    console.info(`Something about ${a} and ${b}`);
+}
+/* Tail comment */
