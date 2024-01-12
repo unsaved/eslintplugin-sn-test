208c5f980a691091ee371a48d86d8c4ca87f1fd3
Blaine Simpson <blaine.simpson@admc.com>
2024-01-12 11:25:16 -0500

    Now successfully tests eslint-plugin-sn v. 3.8.0

--- bad1Scripts/all/sp_widget.client_script/gs.js
+++ bad1Scripts/all/sp_widget.client_script/gs.js
@@ -1,1 +1,13 @@
-if (gs.getUserName() === null) throw new Error("bad");
+function($scope) {
+    "use strict";
+    const fName = "spclient/" + $scope.widget.id + ":" + $scope.$id;
+	console.warn("UPDATE02");
+    console.warn("$scope", $scope);
+	console.info("Executing " + fName + " with this", this);
+	this.thisVar = "I am here";  // To facilitate checking for this exact instance
+	console.info(fName + " inputs", this.inputs);
+	gs.info(fName + " data", this.data);
+	console.info(fName + " options", this.options);
+	$scope.pneu = "new string";
+	console.info("this", this);
+}
