02452406ab21f8e8b58b4cdae3b0ff6821848f41
Blaine Simpson <blaine.simpson@admc.com>
2025-06-20 13:37:39 -0400

    Additional tests and several adjustments for Yokohama accomodation and new use of var-on-top warning

--- bad2Scripts/global/sys_script/noVarOnTop.js
+++ bad2Scripts/global/sys_script/noVarOnTop.js
@@ -1,14 +1,13 @@
 (function() {
-var varsAreAllowed;
-const con = "a val";
 // Test availability of global vars from the different component lists:
 gs.log("a message", "a source");  // coreServerObjects.txt
 gs.log(SNC.CMDBUtil.getTables0("sys_db_object"), "src");  // directly in exports.js
 gs.log(Object.keys(new sn_codesearch.CodeSearch()).length, "src");  // SIScopes
 
-varsAreAllowed = "a val";
+var varsAreAllowed = "a val";
 gs.log(varsAreAllowed, "2nd");
 
+const con = "a val";
 con += "addition";  // Can't test for const changes
 gs.log(con, "2nd");
 })(previous, current);
